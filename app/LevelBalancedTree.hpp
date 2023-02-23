c99600ef7c46fecbe1667bedddb301c3793b176b
Cole <pickeric@uci.edu>
2023-02-23 15:36:02 -0800

    everything besides remove

--- app/LevelBalancedTree.hpp
+++ app/LevelBalancedTree.hpp
@@ -19,6 +19,282 @@ private:
 	// fill in private member data here
 	// If you need to declare private functions, do so here too.
 
+	struct Node
+	{
+		Key key;
+		Value val;
+
+		Node* left = nullptr;
+		Node* right = nullptr;
+		Node* parent = nullptr;
+
+		unsigned leftDis = 1;
+		unsigned rightDis = 1;
+		unsigned level = 1;
+
+	};
+
+	//Deconstructor helper function
+	void decHelp(Node* temp){
+		if (temp == nullptr){
+		return;
+		}
+		decHelp(temp->left);
+		decHelp(temp->right);
+		delete temp;
+	}
+
+	//in order helper
+	void inOrderHelper(Node* current, std::vector<Key> &final) const{
+		if (!current->left){
+			final.push_back(current->key);
+		}
+		else{
+			inOrderHelper(current->left, final);
+			final.push_back(current->key);
+		}
+
+		if (!current->right){
+			return;
+		}
+		else{
+			inOrderHelper(current->right, final);
+		}
+	}
+
+	//pre order helper
+	void preOrderHelper(Node* c, std::vector<Key> &final) const {
+		final.push_back(c->key);
+		if (c->left){
+			preOrderHelper(c->left, final);
+		}
+
+		if (c->right){
+			preOrderHelper(c->right, final);
+		}
+	}
+
+	//post order helper
+	void postOrderHelper(Node* c, std::vector<Key> &final) const {
+		if (c->left){
+			postOrderHelper(c->left, final);
+		}
+
+		if (c->right){
+			postOrderHelper(c->right, final);
+		}
+		final.push_back(c->key);
+	}
+
+	//distance rebalance helper function
+	void sideLevelerHelp(Node* n){
+		if(n->left){
+		n->leftDis = n->level - n->left->level;
+		}
+		else{
+			n->leftDis = n->level;
+		}
+		if (n->right){
+		n->rightDis = n->level - n->right->level;
+		}
+		else{
+			n->rightDis = n->level;
+		}
+	}
+
+
+	//insert balance helper function
+	void insertHelp(Node* cur){
+		if (!cur){
+			return;
+		}
+		Node* oldpar = nullptr;
+		if(cur->parent){
+			oldpar = cur->parent;
+		}
+		
+		//check if left child is on the same level
+		//left
+		if (cur->left and cur->level == cur->left->level){
+
+			if(cur->rightDis ==1){
+				cur->level +=1;
+
+				sideLevelerHelp(cur);
+				if (cur->parent){
+					sideLevelerHelp(cur->parent);
+				}
+				insertHelp(cur->parent);
+			}
+
+			//check if cur can go down
+			//check if right is dist 2
+			else if (cur->rightDis == 2){
+				cur->level -=1;
+
+				cur->parent = cur->left;
+				cur->left= cur->parent->right;
+				cur->parent->right = cur;
+				if (cur->left){
+					cur->left->parent = cur;
+				}
+				if (oldpar){
+					if (oldpar->key < cur->key){
+						oldpar->right = cur->parent;
+						cur->parent->parent=oldpar;
+					}
+					else{
+						oldpar->left = cur->parent;
+						cur->parent->parent=oldpar;
+					}
+				}
+				else{
+					cur->parent->parent = nullptr;
+					this->root = cur->parent;
+				}
+
+				sideLevelerHelp(cur);
+				insertHelp(cur->parent);
+
+			}
+		}
+		//right
+		else if (cur->right and cur->level == cur->right->level){
+
+			if(cur->leftDis ==1){
+				cur->level +=1;
+
+				sideLevelerHelp(cur);
+				if (cur->parent){
+					sideLevelerHelp(cur->parent);
+				}
+				insertHelp(cur->parent);
+			}
+
+			//check if cur can go down
+			//check if right is dist 2
+			else if (cur->leftDis == 2){
+				cur->level -=1;
+
+
+				cur->parent = cur->right;
+				cur->right= cur->parent->left;
+				cur->parent->left = cur;
+				if (cur->right){                                    //dont think i need this in either
+					cur->right->parent = cur;
+				}
+				if (oldpar){
+					if (oldpar->key < cur->key){
+						oldpar->right = cur->parent;
+						cur->parent->parent=oldpar;
+					}
+					else{
+						oldpar->left = cur->parent;
+						cur->parent->parent=oldpar;
+					}
+				}
+				else{
+					cur->parent->parent = nullptr;
+					this->root = cur->parent;
+				}
+
+				sideLevelerHelp(cur);
+				insertHelp(cur->parent);
+
+			}
+		}
+		else if (cur->right and cur->right->left and cur->right->level == cur->right->left->level) {
+			Node* mc = cur->right->left;
+			mc->level += 1;
+			cur -> level -=1;
+
+			//reassign left pointer
+			cur->right = mc->left;
+			if (cur->right){
+				cur->right->parent = cur;
+			}
+			mc->parent->left = mc->right;
+			if (mc->right){
+				mc->right->parent = mc->parent;
+			}
+			//fix mc pointers
+			mc->right = mc->parent;
+			mc->left = cur;
+			mc->parent = oldpar;
+			//fix old par
+			if (oldpar){
+					if (oldpar->key < mc->key){
+						oldpar->right = mc;
+						mc->parent=oldpar;
+					}
+					else{
+						oldpar->left = mc;
+						mc->parent=oldpar;
+					}
+			}
+			else{
+				this->root = mc;
+			}
+			//fix right child
+			mc->right->parent = mc;
+			//fix left child
+			mc->left->parent = mc;
+			sideLevelerHelp(mc);
+			sideLevelerHelp(mc->left);
+			sideLevelerHelp(mc->right);
+		}
+		else if (cur->left and cur->left->right and cur->left->level == cur->left->right->level) {
+			Node* mc = cur->left->right;
+			mc->level += 1;
+			cur -> level -=1;
+
+			//reassign left pointer
+			cur->left = mc->right;
+			if (cur->left){
+				cur->left->parent = cur;
+			}
+			mc->parent->right = mc->left;
+			if (mc->left){
+				mc->left->parent = mc->parent;
+			}
+			//fix mc pointers
+			mc->left = mc->parent;
+			mc->right = cur;
+			mc->parent = oldpar;
+			//fix old par
+			if (oldpar){
+					if (oldpar->key < mc->key){
+						oldpar->right = mc;
+						mc->parent=oldpar;
+					}
+					else{
+						oldpar->left = mc;
+						mc->parent=oldpar;
+					}
+			}
+			else{
+				this->root = mc;
+			}
+			//fix left child
+			mc->left->parent = mc;
+			//fix right child
+			mc->right->parent = mc;
+			sideLevelerHelp(mc);
+			sideLevelerHelp(mc->left);
+			sideLevelerHelp(mc->right);
+		}
+		else{
+			return;
+		}
+		sideLevelerHelp(cur);
+	}
+
+	//Size counter
+	unsigned count;
+	//Root of tree
+	Node* root;
+	
+
 public:
 	LevelBalancedTree();
 
@@ -57,7 +333,6 @@ public:
 	// the grading script will deal with this situation)
 	void insert(const Key & k, const Value & v);
 
-
 	// Deletes the given key from the tree
 	// and performs the balancing operation(s) if needed.
 	// If the key does not exist in the tree, 
@@ -79,32 +354,48 @@ public:
 template<typename Key, typename Value>
 LevelBalancedTree<Key,Value>::LevelBalancedTree()
 {
-
+	this->count = 0;
+	this->root = new Node;
 }
 
 template<typename Key, typename Value>
 LevelBalancedTree<Key,Value>::~LevelBalancedTree()
 {
-
+	decHelp(this->root);
 }
 
 template<typename Key, typename Value>
 size_t LevelBalancedTree<Key, Value>::size() const noexcept
 {
-	return 0; // stub
+	return this->count;
 }
 
 template<typename Key, typename Value>
 bool LevelBalancedTree<Key, Value>::isEmpty() const noexcept
 {
-	return true; // stub
+	if (count == 0){
+		return true;
+	}
+	return false;
 }
 
 
 template<typename Key, typename Value>
 bool LevelBalancedTree<Key, Value>::contains(const Key &k) const noexcept
 {
-	return false; // stub
+	Node* temp = this->root;
+	while (temp){
+		if (temp->key < k){
+			temp = temp->right;
+		}
+		else if (k<temp->key){
+			temp = temp->left;
+		}
+		else if (temp->key==k){
+			return true;
+		}
+	}
+	return false;
 }
 
 
@@ -112,27 +403,110 @@ bool LevelBalancedTree<Key, Value>::contains(const Key &k) const noexcept
 template<typename Key, typename Value>
 Value & LevelBalancedTree<Key, Value>::find(const Key & k)
 {
-	Value v;
-	return v; // not only a stub, but a terrible idea.
+	Node* temp = root;
+	while (temp){
+		if (temp->key < k){
+			temp = temp->right;
+		}
+		else if (k<temp->key){
+			temp = temp->left;
+		}
+		else if (temp->key==k){
+			return temp->val;
+		}
+	}
+	throw ElementNotFoundException("Element not in tree");
 }
 
 template<typename Key, typename Value>
 const Value & LevelBalancedTree<Key, Value>::find(const Key & k) const
 {
-	Value v;
-	return v; // not only a stub, but a terrible idea.
+	Node* temp = root;
+	while (temp){
+		
+		if (temp->key < k){
+			temp = temp->right;
+		}
+		else if (k<temp->key){
+			temp = temp->left;
+		}
+		else if (temp->key==k){
+			const Value &v = temp->val;
+			return v;
+		}
+	}
+	throw ElementNotFoundException("Element not in tree");
 }
 
 template<typename Key, typename Value>
 void LevelBalancedTree<Key, Value>::insert(const Key & k, const Value & v)
 {
+	Node* temp = this->root;
+	if (this->count == 0){
+		this->root->key = k;
+		this->root->val = v;
+	}
+	else{
+		Node* i = new Node;
+		i->val = v;
+		i->key = k;
+
+		while (true){
+			if (k < temp->key){
+				if (temp->left){
+					temp = temp->left;
+				}
+				else{
+					temp->left = i;
+					i->parent = temp;
+					temp->leftDis = 1;
+					break;
+				}
+			}
+			else if (temp->key < k){
+				if (temp->right){
+					temp = temp->right;
+				}
+				else{
+					temp->right = i;
+					i->parent = temp;
+					temp->rightDis = 1;
+					break;
+				}
+			}
+		}
+	}
+	this->count += 1;
+	
+	insertHelp(temp);
+
 	return; 	
 }
 
 template<typename Key, typename Value>
 void LevelBalancedTree<Key, Value>::remove(const Key &k)
+//Code From Zybooks Chapet 7.4
 {
-	return;
+	//find node
+	Node* temp = root;
+	while (temp){
+		if (temp->key < k){
+			temp = temp->right;
+		}
+		else if (k<temp->key){
+			temp = temp->left;
+		}
+		else if (temp->key==k){
+			break;
+		}
+	}
+	if (!temp){
+		return;
+	}
+	//Node found
+	//Leaf Node
+
+
 }
 
 
@@ -140,24 +514,27 @@ void LevelBalancedTree<Key, Value>::remove(const Key &k)
 template<typename Key, typename Value>
 std::vector<Key> LevelBalancedTree<Key, Value>::inOrder() const
 {
-	std::vector<Key> foo;
-	return foo; 
+	std::vector<Key> final;
+	inOrderHelper(this->root, final);
+	return final; 
 }
 
 
 template<typename Key, typename Value>
 std::vector<Key> LevelBalancedTree<Key, Value>::preOrder() const
 {
-	std::vector<Key> foo;
-	return foo; 
+	std::vector<Key> final;
+	preOrderHelper(root, final);
+	return final; 
 }
 
 
 template<typename Key, typename Value>
 std::vector<Key> LevelBalancedTree<Key, Value>::postOrder() const
 {
-	std::vector<Key> foo;
-	return foo; 
+	std::vector<Key> final;
+	postOrderHelper(root, final);
+	return final;
 }
 
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,25 @@
 #include <fstream>
 #include <iostream>
+#include "LevelBalancedTree.hpp"
 
 
 int main()
 {
+LevelBalancedTree<int, std::string> tree;
+	tree.insert(19, "foo");
+	tree.insert(18, "sna");
+	tree.insert(20, "bar");
+    tree.insert(12, "bar");
+	tree.insert(9, "foo");
+	tree.insert(17, "sna");
+    tree.insert(7, "foo");
+	tree.insert(5, "sna");
+    tree.insert(13, "foo");
+	tree.insert(15, "sna");
+    tree.insert(6, "foo");
+	tree.insert(16, "sna");
+    tree.insert(16, "sna");
+	
 
     return 0;
 }
